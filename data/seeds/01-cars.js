1c0f1c070e8c6443b846d40dde3521dcdd4fd8ac
Savannah <savmaxwell@gmail.com>
2022-03-22 11:44:32 -0600

    setup project and built seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const fiats = [
+    {
+        vin: "3C3CFFER4CT376606",
+        make: "fiat",
+        model: "500",
+        mileage: 150000,
+        title: "clean",
+        transmission: "automatic"
+    },
+    {
+        vin: "3C4PFABY0CT249895",
+        make: "fiat",
+        model: "Freemont",
+        mileage: 100000,
+        title: "clean",
+        transmission: "automatic"
+    },
+    {
+        vin: "ZFBCFADH4EZ017858",
+        make: "fiat",
+        model: "500L",
+        mileage: 85000,
+    },
+    {
+        vin: "3C3CFFDR3ET300976",
+        make: "fiat",
+        model: "500c",
+        mileage: 67000,
+        title: "clean",
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate();
+    await knex('cars').insert(fiats)
+}
